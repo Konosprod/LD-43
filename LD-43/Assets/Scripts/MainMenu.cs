f349f53952b7ec265088d8ae7a8bc92ee94019a2
Konosprod <konosprod@free.fr>
2018-12-02 02:39:33 +0100

    Add options and main menu
    Signed-off-by: Konosprod <konosprod@free.fr>

---
+++ LD-43/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    [Header("Main Menu")]
+    public Button startButton;
+    public Button optionsButton;
+    public Button quitButton;
+
+    [Header("Difficulty")]
+    public GameObject panelDiff;
+    public Button buttonEasy;
+    public Button buttonNormal;
+    public Button buttonHard;
+
+    [Header("Options")]
+    public GameObject panelOptions;
+
+    // Use this for initialization
+	void Start () {
+
+        startButton.onClick.AddListener(StartGame);
+        optionsButton.onClick.AddListener(Options);
+        quitButton.onClick.AddListener(QuitGame);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+    }
+
+    private void Options()
+    {
+        panelOptions.SetActive(true);
+    }
+
+    private void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
---
+++ LD-43/Assets/Scripts/OptionPanel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionPanel : MonoBehaviour {
+
+    public Button returnButton;
+
+	// Use this for initialization
+	void Start () {
+        returnButton.onClick.AddListener(Return);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void Return()
+    {
+        SettingsManager._instance.Save();
+        gameObject.SetActive(false);
+    }
+}
